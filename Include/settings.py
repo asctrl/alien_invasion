698e7737ad9819d41c836a44772f607935f09598
asctrl <40906761@qq.com>
2022-09-10 14:29:12 +0800

    update

---
+++ Include/settings.py
@@ -0,0 +1,16 @@
+class Settings():
+
+    def __init__(self):
+        """初始化游戏设置"""
+        #屏幕设置
+        self.screen_width = 1300
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+        self.ship_speed = 1
+        self.bullet_speed = 1
+        self.bullet_width = 100
+        self.bullet_height = 10
+        self.bullet_color = 250, 10, 10
+        self.alien_speed_x = 0.2
+        self.alien_speed_y = 10
+        self.alien_direction = 1
